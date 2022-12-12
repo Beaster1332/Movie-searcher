5057c88d5078f30a111ab89603f10cfcecbdc0f1
Ivan <xspals@mail.ru>
2022-12-12 22:23:13 +0300

    styling movieItem

--- src/components/Movies/MovieItem/MovieItem.jsx
+++ src/components/Movies/MovieItem/MovieItem.jsx
@@ -2,8 +2,33 @@ import React from "react";
 import classes from './MovieItem.module.css';
 
 const MovieItem = (props) => {
+
+    const { nameRu, countries, genres, posterUrlPreview } = props;
+
+    const genresItems = genres.map(g => `${g.genre} `);
+    const countriesItems = countries.map(c => `${c.country} `)
+
     return <div className={classes.movieItem}>
-        aaa
+        <div className={classes.imgHolder}>
+            <img className={classes.img} src={posterUrlPreview} alt={nameRu} />
+        </div>
+        <div className={classes.infoHolder}>
+            <div>
+                <h3>
+                    {nameRu}
+                </h3>
+            </div>
+            <div>
+                <p className={classes.genres}>
+                    {genresItems}
+                </p>
+            </div>
+            <div>
+                <p className={classes.countries}>
+                    {countriesItems}
+                </p>
+            </div>
+        </div>
     </div>
 }
 
